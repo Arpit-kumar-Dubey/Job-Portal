6a53d17d9738f4b6c978ec4e9d759556a67dd1be
AnujChaudhary5911 <anujchh287@gmail.com>
2026-04-17 11:37:38 +0530

    Update script.js

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,1 +1,12 @@
 
+function downloadHTML() {
+    const element = document.documentElement;
+    const html = element.outerHTML;
+    const blob = new Blob([html], { type: 'text/html;charset=utf-8' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'Job_Portal_Documentation.html';
+    link.click();
+    URL.revokeObjectURL(url);
+}
